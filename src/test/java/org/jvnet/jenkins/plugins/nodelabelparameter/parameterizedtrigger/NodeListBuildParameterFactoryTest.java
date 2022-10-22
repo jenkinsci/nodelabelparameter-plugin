1ad069c6a20a4a8fdb938514c99c4b176307dd6c
Jagruti <tiwarijagruti1@gmail.com>
2022-10-22 20:33:28 +0530

    attempt: attempted to write the getParamter test case

--- src/test/java/org/jvnet/jenkins/plugins/nodelabelparameter/parameterizedtrigger/NodeListBuildParameterFactoryTest.java
+++ src/test/java/org/jvnet/jenkins/plugins/nodelabelparameter/parameterizedtrigger/NodeListBuildParameterFactoryTest.java
@@ -1,8 +1,16 @@
 package org.jvnet.jenkins.plugins.nodelabelparameter.parameterizedtrigger;
 
 import org.junit.jupiter.api.Test;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+import org.jenkinsci.plugins.tokenmacro.TokenMacro;
+import hudson.model.TaskListener;
+import hudson.model.AbstractBuild;
+
 
 import static org.hamcrest.Matchers.is;
+
+import java.io.IOException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
@@ -12,5 +20,26 @@ public class NodeListBuildParameterFactoryTest {
         NodeListBuildParameterFactory factory = new NodeListBuildParameterFactory("labelName", "nodeListName");
         assertThat(factory.name, is("labelName"));
         assertThat(factory.nodeListString, is("nodeListName"));
+    }
+
+    @Test
+    void testGetParameters() {
+        AbstractBuild<?, ?> build = new AbstractBuild<?,?>() {
+            
+        };
+        TaskListener listener;
+        try {
+            String labelExpanded = TokenMacro.expandAll(build, listener, labelExpanded);
+        } catch (MacroEvaluationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
