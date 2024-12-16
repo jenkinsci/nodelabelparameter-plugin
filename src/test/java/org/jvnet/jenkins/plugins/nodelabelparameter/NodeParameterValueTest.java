f826eb7e420e0d469d4cf2bb29ae523e99e000ad
Mark Waite <mark.earl.waite@gmail.com>
2024-12-16 13:18:21 -0700

    Use equalsverifier for hashCode and equals tests
    https://jqno.nl/equalsverifier/ is much more thorough in testing the
    equals and hashCode implementations.  Use it rather than crafting separate
    tests of our own.

--- src/test/java/org/jvnet/jenkins/plugins/nodelabelparameter/NodeParameterValueTest.java
+++ src/test/java/org/jvnet/jenkins/plugins/nodelabelparameter/NodeParameterValueTest.java
@@ -1,10 +1,9 @@
 package org.jvnet.jenkins.plugins.nodelabelparameter;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
 import org.junit.Test;
 
 public class NodeParameterValueTest {
@@ -15,26 +14,11 @@ public class NodeParameterValueTest {
     }
 
     @Test
-    public void testHashCode() {
-        NodeParameterValue nvp = new NodeParameterValue("node", "description", "label");
-        NodeParameterValue nvp2 = new NodeParameterValue("node", "description", "label");
-        assertEquals(nvp.hashCode(), nvp2.hashCode());
-
-        NodeParameterValue nvp3 = new NodeParameterValue("node", "description", "label2");
-        assertNotEquals(nvp, nvp3);
-    }
-
-    @Test
-    public void testEquals() {
-        NodeParameterValue nvp = new NodeParameterValue("node", "description", "label");
-        NodeParameterValue nvp2 = new NodeParameterValue("node", "description", "label");
-        assertTrue(nvp.equals(nvp2));
-
-        NodeParameterValue nvp3 = new NodeParameterValue("node", "description", "label2");
-        assertFalse(nvp.equals(nvp3));
-
-        assertFalse(nvp.equals(null));
-        assertFalse(nvp.equals(new Object()));
-        assertTrue(nvp.equals(nvp));
+    public void equalsContract() {
+        EqualsVerifier.forClass(NodeParameterValue.class)
+                .usingGetClass()
+                .suppress(Warning.NONFINAL_FIELDS)
+                .withIgnoredFields("description")
+                .verify();
     }
 }
